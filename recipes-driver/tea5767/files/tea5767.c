b334cfed278ecc7e3650e4410e06820853c74cbe
oussema2232 <36249683+oussema2232@users.noreply.github.com>
2023-03-07 17:20:15 +0100

    Update tea5767.c

--- recipes-driver/tea5767/files/tea5767.c
+++ recipes-driver/tea5767/files/tea5767.c
@@ -1,19 +1,49 @@
-#include <linux/i2c.h>
 #include <linux/kernel.h>
+#include <linux/slab.h>
 #include <linux/module.h>
-#include <linux/videodev2.h>
+#include <linux/init.h>			/* Initdata			*/
+#include <linux/videodev2.h>		/* kernel radio structs		*/
+#include <linux/i2c.h>			/* I2C				*/
 #include <media/v4l2-common.h>
+#include <media/v4l2-ioctl.h>
 #include <media/v4l2-device.h>
 #include <media/v4l2-ctrls.h>
+#include <media/v4l2-event.h>
 
 #define TEA5767_I2C_ADDRESS 0x60
 #define DRIVER_NAME "tea5767"
+struct tea5767_regs {
+	u16 intreg;				/* INTFLAG & INTMSK */
+	u16 frqset;				/* FRQSETMSB & FRQSETLSB */
+	u16 tnctrl;				/* TNCTRL1 & TNCTRL2 */
+	u16 frqchk;				/* FRQCHKMSB & FRQCHKLSB */
+	u16 tunchk;				/* IFCHK & LEVCHK */
+	u16 testreg;				/* TESTBITS & TESTMODE */
+	u16 rdsstat;				/* RDSSTAT1 & RDSSTAT2 */
+	u16 rdslb;				/* RDSLBMSB & RDSLBLSB */
+	u16 rdspb;				/* RDSPBMSB & RDSPBLSB */
+	u16 rdsbc;				/* RDSBBC & RDSGBC */
+	u16 rdsctrl;				/* RDSCTRL1 & RDSCTRL2 */
+	u16 rdsbbl;				/* PAUSEDET & RDSBBL */
+	u16 manid;				/* MANID1 & MANID2 */
+	u16 chipid;				/* CHIPID1 & CHIPID2 */
+} __attribute__ ((packed));
 
+struct tea5767_write_regs {
+	u8 intreg;				/* INTMSK */
+	__be16 frqset;				/* FRQSETMSB & FRQSETLSB */
+	__be16 tnctrl;				/* TNCTRL1 & TNCTRL2 */
+	__be16 testreg;				/* TESTBITS & TESTMODE */
+	__be16 rdsctrl;				/* RDSCTRL1 & RDSCTRL2 */
+	__be16 rdsbbl;				/* PAUSEDET & RDSBBL */
+} __attribute__ ((packed));
 struct tea5767_device {
-    struct v4l2_device v4l2_dev;
-    struct v4l2_ctrl_handler ctrl_handler;
-    struct mutex mutex;
-    struct i2c_client *i2c_client;
+	struct v4l2_device		v4l2_dev;
+	struct v4l2_ctrl_handler	ctrl_handler;
+	struct i2c_client		*i2c_client;
+	struct video_device		vdev;
+	struct tea5767_regs		regs;
+	struct mutex			mutex;
 };
 
 static const struct v4l2_ctrl_ops tea5767_ctrl_ops = {
@@ -23,9 +53,53 @@ static const struct v4l2_ctrl_ops tea5767_ctrl_ops = {
 static int tea5767_i2c_probe(struct i2c_client *client,
                              const struct i2c_device_id *id)
 {
-    printk("probe here  \n");
-    return 0;
+    struct tea5767_device *radio;
+    struct v4l2_device *v4l2_dev;
+    struct v4l2_ctrl_handler *hdl;
+    int ret;
+    printk("ERROR\n");
+    radio = kzalloc(sizeof(struct tea5767_device), GFP_KERNEL);
+    if (!radio)
+        return -ENOMEM;
+     printk("ERROR2\n");
+    v4l2_dev = &radio->v4l2_dev;
+     printk("ERROR3\n");
+    if (!client) {
+    dev_err(&client->dev, "client is NULL\n");
+    printk("ERROR\n");
+    return -EINVAL;
 }
+
+
+    ret = v4l2_device_register(&client->dev, v4l2_dev);
+    if (ret < 0) {
+        v4l2_err(v4l2_dev, "could not register v4l2_device\n");
+        printk("%d",ret);
+        goto errfr;
+    }
+
+    hdl = &radio->ctrl_handler;
+    v4l2_ctrl_handler_init(hdl, 1);
+    v4l2_ctrl_new_std(hdl, &tea5767_ctrl_ops,
+            V4L2_CID_AUDIO_MUTE, 0, 1, 1, 1);
+    v4l2_dev->ctrl_handler = hdl;
+
+    if (hdl->error) {
+        ret = hdl->error;
+        v4l2_err(v4l2_dev, "Could not register controls\n");
+        goto errunreg;
+    }
+
+    return 0;
+
+errunreg:
+    v4l2_ctrl_handler_free(hdl);
+    v4l2_device_unregister(v4l2_dev);
+errfr:
+    kfree(radio);
+    return ret;
+}
+
 
 static int tea5767_i2c_remove(struct i2c_client *client)
 {
@@ -33,7 +107,7 @@ static int tea5767_i2c_remove(struct i2c_client *client)
     return 0;
 }
 static const struct of_device_id tea5767_dt_ids[] = {
-	{ .compatible = "actia,tea5767","brcm,bcm2711",},
+	{ .compatible = "actia,tea5767",},
 	{ }
 };
 MODULE_DEVICE_TABLE(of, tea5767_dt_ids);
@@ -60,7 +134,7 @@ static struct i2c_driver tea5767 = {
 static int __init ModuleInit(void) {
 	struct i2c_client *client;
 	printk("this is init1\n");
-	tea5767_i2c_probe(client, &tea5767_id_table[0]);
+	tea5767_i2c_probe(client, tea5767_id_table);
 	printk("this is init2\n");
 	return i2c_add_driver(&tea5767);
 	}
